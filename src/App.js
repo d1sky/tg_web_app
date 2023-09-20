5cbdb3149109cf7886d5770eff4b0a2095fe2811
d1sky <godasin@gmail.com>
2023-09-20 20:50:54 +0300

    mui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useEffect } from 'react';
 import './App.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 const tg = window.Telegram.WebApp;
 
 function App() {
@@ -14,8 +19,11 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Стройка</h1>
-      <button onClick={onClose}> Закрыть</button>
+      <Typography variant="h3" gutterBottom>
+        Стройка
+      </Typography>
+      <TextField fullWidth label="fullWidth" id="fullWidth" />
+      <Button variant="outlined" onClick={onClose}>Закрыть</Button>
     </div>
   );
 }
