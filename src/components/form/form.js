fc3ebf4d988f2cc2b782e044d6296ee2b9d19223
d1sky <godasin@gmail.com>
2023-09-21 15:14:27 +0300

    button

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,57 +1,61 @@
-import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
-import InputAdornment from '@mui/material/InputAdornment';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import Stack from '@mui/material/Stack';
 import * as React from 'react';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
 
 export default function Form() {
     const [showPassword, setShowPassword] = React.useState(false);
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
-
     return (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-            <div>
-                <FormControl fullWidth sx={{ m: 5 }}>
-                    <InputLabel htmlFor="outlined-adornment-amount">ФИО</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-amount"
-                        label="Amount"
-                    />
-                </FormControl>
-            </div>
-            <div>
-                <FormControl fullWidth sx={{ m: 5 }}>
-                    <InputLabel htmlFor="outlined-adornment-amount">Телефон</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-amount"
-                        label="Amount"
-                    />
-                </FormControl>
-            </div>
-            <div>
-                <FormControl fullWidth sx={{ m: 5 }}>
-                    <InputLabel htmlFor="outlined-adornment-amount">Кадастр</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-amount"
-                        label="Amount"
-                    />
-                </FormControl>
-            </div>
-            <div>
-                <FormControl fullWidth sx={{ m: 5 }}>
-                    <InputLabel htmlFor="outlined-adornment-amount">Населенный пункт</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-amount"
-                        label="Amount"
-                    />
-                </FormControl>
-            </div>
-        </Box>
+        <Stack
+            component="form"
+            sx={{
+                padding: '15px'
+            }}
+            spacing={2}
+            noValidate
+            autoComplete="off"
+        >
+
+            <FormControl fullWidth sx={{ m: 5 }}>
+                <InputLabel htmlFor="outlined-adornment-amount">ФИО</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-amount"
+                    label="Amount"
+                />
+            </FormControl>
+
+            <FormControl fullWidth sx={{ m: 5 }}>
+                <InputLabel htmlFor="outlined-adornment-amount">Телефон</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-amount"
+                    label="Amount"
+                />
+            </FormControl>
+
+            <FormControl fullWidth sx={{ m: 5 }}>
+                <InputLabel htmlFor="outlined-adornment-amount">Кадастр</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-amount"
+                    label="Amount"
+                />
+            </FormControl>
+
+            <FormControl fullWidth sx={{ m: 5 }}>
+                <InputLabel htmlFor="outlined-adornment-amount">Населенный пункт</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-amount"
+                    label="Amount"
+                />
+            </FormControl>
+
+            <FormControlLabel control={<Switch defaultChecked />} label="Созадть выезд на пробы" />
+
+            <Button variant="contained" size="large">Сохранить</Button>
+        </Stack>
     );
 }
